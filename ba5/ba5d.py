fb6c86619633c89bb499548f52f1e673dce6c0a3
jigsaw-snu <sglee0723@snu.ac.kr>
2022-09-26 18:39:20 +0900

    adding ba5d.py solution (unfinished)

--- ba5/ba5d.py
+++ ba5/ba5d.py
@@ -1,39 +1,38 @@
-import sys
+import re, sys
 import numpy as np
+from tqdm import tqdm
+
+
+class Node:
+    def __init__(self):
+        self.max_val = 0
+        self.innode = ''
 
 
 '''
     <InputParser>
-    Parse input data and save it to a dictionary
+    Parse input data and save it into a dictionary
 '''
 def InputParser(file_path: str) -> dict:
     params = dict()
 
     with open(file_path, 'r') as file:
-        # first line indicates source node
+        # first and second line refers to source and sink node
         line = file.readline().rstrip()
         params['source'] = int(line)
 
-        # second line indicates sink node
         line = file.readline().rstrip()
         params['sink'] = int(line)
 
-        # from 3rd line, indicates weight information
-        # (source)->(sink):(weight)
-        # params['weights'] = weighted adjacency matrix
+        # from third line, it refers to weight information
+        # for the ease of access, let's store this data in numpy array
         #
-        # **Check**
-        # 1. how many nodes are there? --> shape(mat) = (num_nodes) x (num_nodes)
-        #    -> we consider number of nodes as size of biggest node
-        #    -> check biggest node
-        # 1-1. set biggest node = max(source_node, sink_node)
-        #      -> source node > sink node might be possible
-        # 1-2. while iterating each line, renew biggest node
-        #      -> we cannot guarantee source_node or sink_node is the biggest node
-        # 2. construct zero matrix with len(biggest_node)
-        # 3. substitue each weight to a given position of matrix
-        biggest_node = params['sink']
-        weight_list = []
+        # Assumptions
+        # 1. node[i] is not linked to node[j] for all node[i] > node[j]
+        #    --> only smaller node to bigger node is allowed
+        # 2. according to 1), let us consider sink node as the maximum node
+        params['weight'] = []
+        params['minimap'] = np.zeros(shape=(params['sink']+1, params['sink']+1))
 
         while True:
             line = file.readline().rstrip()
@@ -41,37 +40,56 @@ def InputParser(file_path: str) -> dict:
             if not line:
                 break
 
-            src, dst, wt = map(int, [
-                line[:line.index('-')],
-                line[line.index('>')+1:line.index(':')],
-                line[line.index(':')+1:]
-                    ])
+            src, dst, wt = list(map(int, re.findall(r'\d', line)))
+            params['weight'].append((src, dst, wt))
 
-            if biggest_node < src:
-                biggest_node = src
-
-            if biggest_node < dst:
-                biggest_node = dst
-
-            weight_list.append((src, dst, wt))
-
-        # remember node starts from 0 and biggest_node must be included in the matrix
-        params['weights'] = np.zeros(shape=(biggest_node+1, biggest_node+1))
-        
-        for weight in weight_list:
-            src, dst, wt = weight
-            
-            # we set row as src, col as dst
-            params['weights'][src, dst] = wt
+            if src <= params['sink'] and dst <= params['sink']:
+                params['minimap'][src, dst] = wt
+            else:
+                continue
 
     return params
- 
 
+
+'''
+    <PathSelector>
+    Select Best Path from source node to sink node
+'''
+def PathSelector(src: int, dst: int, weight: list) -> list:
+    nodes = [Node()] * (dst+1)  # to make list index same as iterater
+
+    for i in range(src+1, dst+1):
+        print('index :', i)
+        innodes = [x for x in weight if x[1] == i]
+        print(nodes[innodes[0][0]].max_val)
+
+        if len(innodes) < 1:
+            continue
+
+        # score = max(prev_node scores + weights)
+        innodes_val = [nodes[innode[0]].max_val + innode[2] for innode in innodes]
+
+        print(innodes)
+        print(innodes_val)
+
+        nodes[i].max_val = max(innodes_val)
+        nodes[i].innode = innodes[innodes_val.index(nodes[i].max_val)][0]
+
+    res = [
+        nodes[dst].max_val,
+        '->'.join([str(node.innode) for node in nodes[src:]])
+    ]
+
+    return res
 
 
 if __name__ == '__main__':
     # test out InputParser
     params = InputParser(sys.argv[1])
+
     print(params['source'])
     print(params['sink'])
-    print(params['weights'])
+    print(params['weight'], '\n')
+
+    path_selected = PathSelector(params['source'], params['sink'], params['weight'])
+    print(path_selected[0], path_selected[1], sep='\n')
