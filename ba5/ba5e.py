ead051927b912eacd985f2ce7428e436eaae5a61
jigsaw-snu <sglee0723@snu.ac.kr>
2022-09-28 12:17:03 +0900

    adding ba5e code still on going

--- ba5/ba5e.py
+++ ba5/ba5e.py
@@ -1,5 +1,6 @@
 import sys, re
 import pandas as pd
+import numpy as np
 
 
 '''
@@ -7,22 +8,26 @@ import pandas as pd
     Read Blosum matrix in txt form and save into dataframe form
 '''
 def BlosumParser(blosum_path: str) -> pd.DataFrame:
-    blosum = []
+    contents = []
     idx = []
-    
+
     with open(blosum_path, 'r') as blosum:
         while True:
             line = blosum.readline().strip()
 
+            if '#' in line:
+                continue
+
             if not line:
                 break
-            
+
             if not re.findall(r'\d+', line):
-                idx += line
-            
-            blosum.append(list(map(int, line[1:]))
-        
-    res = pd.DataFrame(blosum, columns=idx, index=idx)
+                idx += line.split()
+                continue
+
+            contents.append(list(map(int, line.split()[1:])))
+
+    res = pd.DataFrame(contents, columns=idx, index=idx)
 
     return res
 
@@ -33,7 +38,7 @@ def BlosumParser(blosum_path: str) -> pd.DataFrame:
 '''
 def InputParser(file_path: str) -> dict:
     params = dict()
-    params['seq'] = []
+    params['sequence'] = []
 
     with open(file_path, 'r') as file:
         while True:
@@ -42,16 +47,34 @@ def InputParser(file_path: str) -> dict:
             if not line:
                 break
 
-            params['seq'].append(line)
+            params['sequence'].append(line)
 
     return params
 
-'''
-'''
-def GlobalAligner(seq1: str, seq2: str, blosum: pd.DataFrame) -> list:
-    pass
 
+'''
+    <GlobalAligner>
+    Find global alignment between two given sequences
+'''
+def GlobalAligner(sequence: list, blosum: pd.DataFrame, penalty: int) -> list:
+    score_matrix = np.zeros(shape=(len(sequence[0])+1, len(sequence[1])+1))
+    print(score_matrix)
+
+    # first row and first column is zero padded
+    for i in range(1, len(sequence[0])+1):
+        for j in range(1, len(sequence[1])+1):
+            score_matrix[i, j] = max([
+                score_matrix[i-1, j-1] + \
+                    blosum[sequence[0][i-1]][sequence[1][j-1]],
+                score_matrix[i-1, j] - penalty,
+                score_matrix[i, j-1] - penalty
+            ])
+
+    print(score_matrix)
 
 if __name__ == '__main__':
-    blossum = BlosumParser(sys.argv[2])
+    blosum = BlosumParser(sys.argv[2])
     params = InputParser(sys.argv[1])
+    align = GlobalAligner(params['sequence'], blosum = blosum, penalty=5)
+
+   # print(align[0], align[1], sep='\n')
