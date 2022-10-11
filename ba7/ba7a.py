16897bb3d6c7673e03793b7b448cde07b6e79300
jigsaw-snu <sglee0723@snu.ac.kr>
2022-10-11 16:35:03 +0900

    solving ba7a. BFS too slow.. need optimization

--- ba7/ba7a.py
+++ ba7/ba7a.py
@@ -1,16 +1,24 @@
-import sys
+import time
+import sys, copy
+from tqdm import tqdm
+from collections import deque
 
 
 '''
     <InputParser>
 '''
 def InputParser(input_path: str) -> list:
-    ret = [0, dict()]
+    #ret = [0, dict()]
+
+    # [nr_leaf, {src :[dst]}, {(src, dst) : weight}, [leaf_nodes]]
+    ret = [0, dict(), dict(), []]
 
     with open(input_path, 'r') as file:
         # first line refers to number of leaf nodes
         nr_leaf = int(file.readline().rstrip())
         ret[0] = nr_leaf
+
+        blacklist = []
 
         while True:
             line = file.readline().rstrip()
@@ -23,23 +31,79 @@ def InputParser(input_path: str) -> list:
             weight = int(line[line.index(':')+1:])
 
             if prev_node in ret[1].keys():
-                ret[1][prev_node].append({next_node:weight})
+                ret[1][prev_node].append(next_node)
             else:
-                ret[1][prev_node] = [{next_node : weight}]
+                ret[1][prev_node] = [next_node]
+
+            ret[2][(prev_node, next_node)] = weight
+
+            if (prev_node not in ret[3]) and (prev_node not in blacklist):
+                ret[3].append(prev_node)
+            else:
+                # multiple src nodes means not leaf node
+                blacklist.append(prev_node)
+
+        # only leaf nodes should be left in ret[3]
+        ret[3] = [x for x in ret[3] if x not in blacklist]
 
     return ret
 
 
-def GetDistance(nr_leaf: int, node_info: dict) -> None:
-    for node in node_info.keys():
-        near_node = [key for entry in node_info[node] \
-                     for (key, value) in entry.items() \
-                     if key in range(nr_leaf)]
-        print('node:', node, 'near_node:', near_node)
+'''
+    <FindPathBFS>
+'''
+def FindPathBFS(src: int, dst: int, node_info: list) -> list:
+    path_history = deque([])  # save all path history in here
+
+    while path_history:
+        #print(path_history)
+        # path_history is a queue, since we want to perform BFS!
+        cur_path = path_history.popleft()  # get first path from path_history
+        #print(cur_path)
+        last_node = cur_path[-1]  # get last node of path
+
+        if last_node == dst:  # dst found!
+            return cur_path
+
+        try:
+            near_nodes = node_info[1][last_node]
+        except:
+            near_nodes = []
+
+        for near_node in near_nodes:  # spawn new path and add to path_history
+            tmp_path = copy.deepcopy(cur_path)
+            tmp_path.append(near_node)
+            path_history.append(tmp_path)
+
+
+'''
+    <GetDistanceMatrix>
+'''
+def GetDistanceMatrix(node_info: list) -> None:
+    ret_matrix = [[0 for _ in range(node_info[0])] for _ in range(node_info[0])]
+
+    for i in tqdm(range(node_info[0])):
+        for j in range(node_info[0]):
+            if i == j:
+                ret_matrix[i][j] = str(ret_matrix[i][j])
+                continue
+            
+            weight_sum = 0
+            node_path = FindPathBFS(node_info[3][i], node_info[3][j], node_info)
+            print('Path', i, 'to', j, ':', node_path)
+
+            for k in range(len(node_path)-1):
+                weight_sum += node_info[2][(node_path[k], node_path[k+1])]
+
+            ret_matrix[i][j] = str(weight_sum)
+    
+    ret_matrix = [' '.join(ret_matrix[i]) for i in range(node_info[0])]
+
+    print('\n'.join(ret_matrix))
 
 
 if __name__ == '__main__':
     params = InputParser(sys.argv[1])
-    print(params[0], params[1], sep='\n')
+    #print(params[0], params[1], params[2], params[3], '\n', sep='\n')
 
-    GetDistance(params[0], params[1])
+    GetDistanceMatrix(params)
